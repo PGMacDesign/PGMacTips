4e4f8c73419876e42da13554d352cf165ef7135d
Patrick MacDowell <PGMacDesign@gmail.com>
2018-07-06 14:33:59 -0700

    Adding in authentication utilities. Working on fingerprint verification stuff

---
+++ library/src/main/java/com/pgmacdesign/pgmactips/authenticationutilities/FingerprintVerification.java
@@ -0,0 +1,14 @@
+package com.pgmacdesign.pgmactips.authenticationutilities;
+
+/**
+ * A class for managing Fingerprint login credentials
+ * This is required to add to the manifest (Though true can be false to make it optional):
+ <uses-feature android:name="android.hardware.fingerprint"
+     android:required="true"/>
+ <uses-permission
+     android:name="android.permission.USE_FINGERPRINT" />
+ * Pulling from: https://www.androidauthority.com/how-to-add-fingerprint-authentication-to-your-android-app-747304/
+ * Created by pmacdowell on 7/6/2018.
+ */
+public class FingerprintVerification {
+}
