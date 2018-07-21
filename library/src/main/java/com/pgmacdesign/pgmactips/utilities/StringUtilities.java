e43bd1225b679786058b14199bb43f57ba9f0cda
Patrick MacDowell <PGMacDesign@users.noreply.github.com>
2018-07-20 17:21:06 -0700

    Update StringUtilities.java

--- library/src/main/java/com/pgmacdesign/pgmactips/utilities/StringUtilities.java
+++ library/src/main/java/com/pgmacdesign/pgmactips/utilities/StringUtilities.java
@@ -1759,5 +1759,35 @@ public class StringUtilities {
         sb = sb.reverse();
         return sb.toString();
     }
+    
+    //Only used in this class{@link StringUtilities#calculateLevenshteinDistance}
+    private static int costOfSubstitution(char a, char b) {
+        return a == b ? 0 : 1;
+    }
+    
+    /**
+     * Calculate the Levenshtein Distance between 2 Strings.
+     * See http://www.baeldung.com/java-levenshtein-distance for more info
+     * @param x String 1 (IE Spot)
+     * @param y String 2 (IE Shot)
+     * @return int distance (IE 1 for the 2 strings passed above)
+     */
+    public static int calculateLevenshteinDistance(String x, String y) {
+        int[][] dp = new int[x.length() + 1][y.length() + 1];
+        for (int i = 0; i <= x.length(); i++) {
+            for (int j = 0; j <= y.length(); j++) {
+                if (i == 0) {
+                    dp[i][j] = j;
+                } else if (j == 0) {
+                    dp[i][j] = i;
+                } else {
+                    dp[i][j] = Utilities.getMinimum(
+                            dp[i - 1][j - 1] + costOfSubstitution(x.charAt(i - 1),
+                                    y.charAt(j - 1)), dp[i - 1][j] + 1, dp[i][j - 1] + 1);
+                }
+            }
+        }
+        return dp[x.length()][y.length()];
+    }    
 }
 
